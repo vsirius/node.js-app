d1d45c38eb7b57d79ccce1dc625a32c82d27360e
Ubuntu <ubuntu@ip-172-31-24-190.eu-central-1.compute.internal>
2017-05-30 17:39:03 +0300

    all

--- client_5.js
+++ client_5.js
@@ -2,14 +2,14 @@
  var requestLoop = setInterval(function(){
  var mysql      = require('mysql');
  var connection = mysql.createConnection({
-  host     : '172.17.0.2',
+  host     : 'application-db',
   user     : 'app',
   password : 'Ws0823~LL',
   database : 'application'
 });
 
 
-request('http://54.93.38.141/ping', function (error, response, body) {
+request('http://application-python-server/ping', function (error, response, body) {
 var obj = JSON.parse(body);
 connection.connect();
 connection.query('insert into events (service,datetime,state) values ("application-nodejs-client","' + obj.datetime +'" ,"' + obj.status + '")', function(error, result, fields){
